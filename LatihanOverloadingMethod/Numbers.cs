f61bc7109f7ec21fdf5f0f5c25c103226b53f942
ASUS <ASUS@RIFANAZIS>
2022-04-09 22:50:19 +0700

    Add project files.

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return number2;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (int)number3;
+        }
+    }
+}
